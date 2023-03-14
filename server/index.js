2a95b0790ae6a01985136a835b6585ce62465efa
Zazil Gomz <zazilgomez@Zazils-MacBook-Air.local>
2023-03-14 12:09:22 -0700

    add multiplayer support

--- server/index.js
+++ server/index.js
@@ -15,23 +15,49 @@ const io = new Server(server, {
   },
 });
 
-// TODO: User joins a room from the home page
-// TODO: Game doesn't start until two players have entered room
-// TODO: Emit Move_Piece from front end and listen to emit on backend - then emit Push_Move on backend and listen to emit on frontend
-// TODO:
+// room registry
+const rooms = {};
 
 // Listen to user connection
 io.on("connection", (socket) => {
   console.log(`User connected: ${socket.id}`);
+
+  socket.on("resetGame", (data) => {
+    var room = rooms[data.roomId];
+    console.log("received resetGame message",data,room)
+    io.emit("broadcastResetGame", room);
+  });
+
   socket.on("join_room", (data) => {
     socket.join(data);
+    var room = rooms[data.roomId];
+    if (!room) {
+      rooms[data.roomId] = {
+        roomId : data.roomId,
+        host : data.playerId,
+        hostColor : "W",
+      };
+      room = rooms[data.roomId];
+      console.log(`room ${data.roomId} created by playerId ${data.playerId}`,room)
+    } else {
+      if (room.guest == data.playerId || room.host == data.playerId) {
+        console.log(`player ${data.playerId} already joined the room before`)
+      } 
+      else if (!room.guest) {
+        room.guest = data.playerId;
+        room.guestColor = "B";
+        console.log(`room ${data.roomId} joined by guest playerId ${data.playerId}`,room)
+      }
+    }
+    io.emit("broadcastRoom", room);
+    console.log("bradcast join room updates")
   });
 
   // listen to onPieceMoved function
   // first argument is the identifier, so make sure to match to emit on frontend
-  socket.on("Move_Piece", (data) => {
+  socket.on("message", (data) => {
     console.log("Move recieved", data);
-    io.emit("Push_Move", data);
+    io.emit("broadcastMove", data);
   });
 
   // CHAT
